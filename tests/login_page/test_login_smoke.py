75f1c2a10d6cf6542652d805bee930239cf67f1f
orenrimer <76216721+orenrimer@users.noreply.github.com>
2020-12-27 11:27:57 +0200

    Update test_login_smoke.py

--- tests/login_page/test_login_smoke.py
+++ tests/login_page/test_login_smoke.py
@@ -11,15 +11,7 @@ class TestLogin:
     def setup(self, driver_setup):
         self.login_page = LoginPage(driver_setup)
         self.login_page.goto()
-
-    @pytest.mark.order(3)
-    @pytest.mark.tcid2
-    def test_login_valid_user(self, setup):
-        self.login_page.login()
-        assert self.login_page.verify_logged_in()
-        if AssertionError:
-            self.login_page.driver.take_screenshot(name="test_login_valid_user")
-
+        
     @pytest.mark.order(1)
     @pytest.mark.tcid3
     def test_login_invalid_email(self, setup):
@@ -28,7 +20,7 @@ class TestLogin:
         assert self.login_page.verify_login_failed()
         if AssertionError:
             self.login_page.driver.take_screenshot(name="test_login_invalid_email")
-
+     
 
     @pytest.mark.order(2)
     @pytest.mark.tcid4
@@ -37,4 +29,15 @@ class TestLogin:
         self.login_page.login(password=password)
         assert self.login_page.verify_login_failed()
         if AssertionError:
-            self.login_page.driver.take_screenshot(name="test_login_invalid_password")
+            self.login_page.driver.take_screenshot(name="test_login_invalid_password")       
+            
+            
+    @pytest.mark.order(3)
+    @pytest.mark.tcid2
+    def test_login_valid_user(self, setup):
+        self.login_page.login()
+        assert self.login_page.verify_logged_in()
+        if AssertionError:
+            self.login_page.driver.take_screenshot(name="test_login_valid_user")
+
+  
