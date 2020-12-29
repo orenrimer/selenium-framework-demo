6474e01b141f429e496c67120b998120547786f6
Oren Rimer <76216721+orenrimer@users.noreply.github.com>
2020-12-29 11:34:24 +0200

    Update payment_page.py

--- pages/checkout_pages/payment_page.py
+++ pages/checkout_pages/payment_page.py
@@ -1,85 +1,54 @@
-import datetime
-from selenium.webdriver.common.by import By
-from pages.base_page import BasePage
+import os
+import pytest
+import unittest
+from ddt import ddt, data, unpack
+from pages.cart_page import CartPage
+from pages.checkout_pages.delivery_page import DeliveryPage
+from pages.checkout_pages.payment_page import PaymentPage
+from pages.login_page import LoginPage
+from pages.product_page import ProductPage
+from pages.results_page import ResultsPage
+from utilities.read_data import getCSVData
 
 
-
-class PaymentPage(BasePage):
-
-    # locators
-    CHANGE_ADDRESS_BTN = (By.XPATH, "//button[contains(text(),'Change')]")
-    SAME_AS_DELIVERY_CHECKBOX = (By.ID, "deliveryAsBilling-checkbox")
-    PAYMENT_OPTIONS_BTN = (By.XPATH, "//button[@class='PaymentMethodOption-button'][@value='{0}']")
-    SAVE_PAYMENT_DETAILS_CHECKBOX = (By.ID, 'SavePaymentDetails-checkbox')
-    TOTAL_PRICE = (By.XPATH, "//div[contains(@class, 'SimpleTotals-total')]//span[@class='SimpleTotals-groupRight'][1]")
-    PAY_BTN = (By.XPATH, "//button[contains(@class, 'Button PaymentBtnWithTC-paynow')]")
-    CARD_NUM_FIELD = (By.ID, "cardNumber-tel")
-    SELECT_EXPIRY_MONTH = (By.ID, "expiryMonth")
-    SELECT_EXPIRY_YEAR = (By.ID, "expiryYear")
-    CVV_FIELD = (By.ID, "cvv-tel")
+pytestmark = [pytest.mark.smoke, pytest.mark.checkout, pytest.mark.deliverypage]
 
 
-    def __init__(self, driver):
-        super().__init__(driver)
+@ddt
+class TestPayWithCreditCard(unittest.TestCase):
 
-    def choose_payment_options(self, payment_option):
-        payment_option = payment_option.lower()
-        if payment_option == "card":
-            self.PAYMENT_OPTIONS_BTN[1].format('CARD')
-            self.driver.element_click(self.PAYMENT_OPTIONS_BTN)
-        elif payment_option == "paypal":
-            self.PAYMENT_OPTIONS_BTN[1].format('PYPAL')
-            self.driver.element_click(self.PAYMENT_OPTIONS_BTN)
-        elif payment_option == "account card":
-            self.PAYMENT_OPTIONS_BTN[1].format('ACCNT')
-            self.driver.element_click(self.PAYMENT_OPTIONS_BTN)
-        elif payment_option == "klrna":
-            self.PAYMENT_OPTIONS_BTN[1].format('KLRNA')
-            self.driver.element_click(self.PAYMENT_OPTIONS_BTN)
-        elif payment_option == "clearpay":
-            self.PAYMENT_OPTIONS_BTN[1].format('CLRPY')
-            self.driver.element_click(self.PAYMENT_OPTIONS_BTN)
-        else:
-            raise Exception(f"Invalid parameter 'payment_option':: {payment_option}. "
-                            f"expected 'card', 'paypal, 'account card', 'klrna' or 'clearpay'.")
+    root = os.path.dirname(os.path.dirname(__file__))
+    data_path = os.path.join(root, "..", "test_data", "CCRecords.csv")
 
-    def enter_card_num(self, card_num):
-        self.driver.element_send_keys(card_num, self.CARD_NUM_FIELD)
+    @pytest.fixture()
+    def setup(self, driver_setup):
+        self.login_page = LoginPage(driver_setup)
+        self.results_page = ResultsPage(driver_setup)
+        self.product_page = ProductPage(driver_setup)
+        self.cart = CartPage(driver_setup)
+        self.payment_page = PaymentPage(driver_setup)
+        self.delivery_page = DeliveryPage(driver_setup)
+        self.product = "jeans"
 
-    def enter_card_cvv(self, cvv):
-        self.driver.element_send_keys(cvv, self.CVV_FIELD)
 
-    def select_card_exp_date(self, expiry_date):
-        expiry_month = expiry_date[:3]
-        if 1 <= expiry_month <= 12:
-            select_exp_month = self.driver.element_select(self.SELECT_EXPIRY_MONTH)
-            select_exp_month.select_by_index(expiry_month-1)
-        else:
-            raise Exception(f"Invalid parameter 'expiry_month':: {str(expiry_month)}, must be an integer between 1 and 12.")
+    @pytest.fixture()
+    def preconditions(self, setup):
+        self.login_page.goto()
+        self.login_page.login()
+        self.results_page.goto_product(self.product)
+        self.product_page.add_to_bag(select_type="dropdown")
+        self.cart.goto()
+        self.cart.goto_checkout_page()
+        self.delivery_page.enter_full_delivery_details()
 
-        expiry_year = expiry_date[3:]
-        now = datetime.datetime.now()
-        curr_year = now.year
-        if expiry_year >= curr_year:
-            select_exp_month = self.driver.element_select(self.SELECT_EXPIRY_MONTH)
-            select_exp_month.select_by_index(expiry_month-1)
-        else:
-            raise Exception(f"Invalid parameter 'expiry_year':: {str(expiry_month)}, card id expired.")
 
-    def fill_card_details(self, curd_num, expiry_date, cvv):
-        self.choose_payment_options(payment_option='card')
-        self.enter_card_num(curd_num)
-        self.select_card_exp_date(expiry_date)
-        self.enter_card_cvv(cvv)
-
-    def place_order(self, payment_options, curd_num, expiry_date, cvv):
-        self.choose_payment_options(payment_options)
-        if payment_options == "card" or payment_options == 'account card':
-            self.fill_card_details(curd_num, expiry_date, cvv)
-        self.driver.element_click(self.PAY_BTN)
-
-    def verify_successful_payment(self):
-        return self.verify_page_title_contains('Thank You')
-
-    def verify_invalid_card_details(self):
-        return self.driver.element_get_attribute('aria-disabled', self.PAY_BTN) == "true"
+    @data(*getCSVData(data_path))
+    @unpack
+    @pytest.mark.tcid30
+    @pytest.mark.usefixtures("setup", "preconditions")
+    def test_pay_with_valid_card(self, ccNum, ccCVV, ccExp):
+        self.payment_page.place_order(payment_options="card", curd_num=ccNum,
+                                      expiry_date=ccExp, cvv=ccCVV)
+        assert self.payment_page.verify_successful_payment()
+        if AssertionError:
+            self.delivery_page.driver.take_screenshot(name="test_pay_with_valid_card")
